a31e31e28c17e75789d9d021c121cc21f27a301f
Evelyn Correa <evelynfcorrea@gmail.com>
2022-10-28 19:15:05 -0300

    quem pensa e faz page

--- src/components/person-card/index.js
+++ src/components/person-card/index.js
@@ -4,14 +4,14 @@ import { InfoDivider, LearnMoreButton } from './style';
 import AddIcon from '@mui/icons-material/Add';
 
 export const PersonCard = ({ name, img, country, description, personClick, section, id }) => (
-    <Box width="200px" height="400px" marginX="30px" key={id}>
+    <Box width="200px" height="350px" marginX="30px" key={id}>
         <img width="150px" src={img} alt={name} />
         <Box height="65px" display="flex" alignItems="center">
             <Typography variant="h3" color={colors.blueLighter} width="80%" margin="auto">{name}</Typography>
         </Box>
         <InfoDivider />
         {country && <Typography variant="body1bold">{country}</Typography>}
-        <Typography height="150px" textOverflow="ellipsis" overflow="hidden">{description}</Typography>
+        <Typography height="120px" textOverflow="ellipsis" overflow="hidden">{description}</Typography>
         <br />
         <LearnMoreButton
             onClick={() => personClick(section, id)}
